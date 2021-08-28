d6b0ca2489c749534fefa965145952a5e4d7c5e6
Eurynunez <20209380@itla.edu.do>
2021-08-28 12:47:57 -0400

    Finalizado

---
+++ DataBase/Singleton/RepositorioPacienteFiltrado.cs
@@ -0,0 +1,21 @@
+﻿using DataBase.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.RepositorioUsuario
+{
+    public class RepositorioPacienteFiltrado
+    {
+        public static RepositorioPacienteFiltrado Instancia { get; } = new RepositorioPacienteFiltrado();
+
+        public List<Paciente> PacienteFiltrado = new List<Paciente>();
+
+        private RepositorioPacienteFiltrado()
+        {
+
+        }
+    }
+}
---
+++ DataBase/Singleton/RepositorioUsuarioLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataBase.Modelo;
+
+namespace DataBase.RepositorioUsuario
+{
+    public class RepositorioUsuarioLogin
+    {
+        public static RepositorioUsuarioLogin Instancia { get; } = new RepositorioUsuarioLogin();
+
+        public List<Usuario> UsuarioLogin = new List<Usuario>();
+
+        private RepositorioUsuarioLogin()
+        {
+
+        }
+    }
+}
